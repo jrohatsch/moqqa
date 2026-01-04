f38c318aefe4aa16691623c3b67315f23c558f43
Johannes Rohatsch <johannes.rohatsch@gmail.com>
2026-01-04 23:00:40 +0100

    render svg images

--- src/main/java/com/github/jrohatsch/moqqa/components/PathItemRenderer.java
+++ src/main/java/com/github/jrohatsch/moqqa/components/PathItemRenderer.java
@@ -3,8 +3,6 @@ package com.github.jrohatsch.moqqa.components;
 
 import com.github.jrohatsch.moqqa.domain.PathListItem;
 import com.github.jrohatsch.moqqa.utils.IconUtils;
-import org.apache.commons.lang3.math.NumberUtils;
-import tools.jackson.databind.ObjectMapper;
 
 import javax.swing.*;
 import java.awt.*;
@@ -12,19 +10,12 @@ import java.awt.*;
 public class PathItemRenderer extends DefaultListCellRenderer {
 
     private String mapImage(PathListItem item) {
-        if (item.childTopics() > 0) {
-            return "cube-solid-full-48x48.png";
-        } else if (item.value().isPresent()) {
-            String value = item.value().get();
-
-            if (value.equalsIgnoreCase("true") || value.equalsIgnoreCase("false")) {
-                return "code-commit-solid-full-48x48.png";
-            } else if (NumberUtils.isCreatable(value)) {
-                return "wave-square-solid-full-48x48.png";
-            }
-        }
-
-        return "align-left-solid-full-48x48.png";
+        return switch (item.type()) {
+            case PARENT -> "folder-regular-full.svg";
+            case TEXT -> "bars-staggered-solid-full.svg";
+            case BOOL -> "square-binary-solid-full.svg";
+            case NUMBER -> "chart-line-solid-full.svg";
+        };
     }
 
     @Override
--- src/main/java/com/github/jrohatsch/moqqa/utils/IconUtils.java
+++ src/main/java/com/github/jrohatsch/moqqa/utils/IconUtils.java
@@ -1,7 +1,13 @@
 package com.github.jrohatsch.moqqa.utils;
 
+import com.github.weisj.jsvg.SVGDocument;
+import com.github.weisj.jsvg.attributes.ViewBox;
+import com.github.weisj.jsvg.parser.SVGLoader;
+
 import javax.swing.*;
 import java.awt.*;
+
+import java.awt.image.BufferedImage;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
@@ -9,21 +15,39 @@ import java.util.Map;
 public class IconUtils {
     private static IconUtils instance;
     private Map<String, Icon> loaded;
+    private SVGLoader svgLoader;
 
     private static IconUtils getInstance() {
         if (instance == null) {
             instance = new IconUtils();
             instance.loaded = new HashMap<>();
+            instance.svgLoader = new SVGLoader();
         }
         return instance;
     }
 
-    private String absolutePath(String imageName) {
-        URL url = IconUtils.class.getResource("/images/%s".formatted(imageName));
-        if (url == null) {
-            throw new RuntimeException("Could not find image %s!".formatted(imageName));
+    public static Image getSVGImage(URL url, int width, int height) {
+        SVGLoader loader = getInstance().svgLoader;
+        SVGDocument document = loader.load(url);
+
+        if (document == null) {
+            return null;
         }
-        return url.getPath();
+
+        // create empty transparent image
+        BufferedImage image = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+        Graphics2D g = image.createGraphics();
+
+        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BICUBIC);
+        g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+        g.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS, RenderingHints.VALUE_FRACTIONALMETRICS_ON);
+
+        // render svg element on image
+        document.render(null, g, new ViewBox(0,0, width,height));
+
+        g.dispose();
+        return image;
     }
 
     public static Icon get(String imageName, Dimension dimension) {
@@ -34,8 +58,7 @@ public class IconUtils {
         }
 
         System.out.printf("loading image %s with dimension %s%n",imageName, dimension);
-        Image image = new ImageIcon(instance.absolutePath(imageName)).getImage();
-        image = image.getScaledInstance(dimension.width,dimension.height,0);
+        var image = getSVGImage(IconUtils.class.getResource("/images/" + imageName), 24, 24);
 
         instance.loaded.put(imageName + dimension, new ImageIcon(image));
 
